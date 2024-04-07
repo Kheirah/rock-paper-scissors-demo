5117c633ed2f0640c4a2d7a6a3bfbdd78ff79dc3
Ernst Stolz <eric@spiritsdontfly.com>
2024-04-07 16:47:06 +0200

    remove logos, add options

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import scissors from "./assets/scissors.svg";
+import "./App.css";
+
+const options = ["✊", "✌️", "✋"];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={scissors} className="logo" alt="Scissors icon" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>Player vs. Computer</h1>
+      <div>
+        Choose your option
+        {options.map((option) => (
+          <button key={option}>{option}</button>
+        ))}
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Choose an option and see if you won against the computer.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
