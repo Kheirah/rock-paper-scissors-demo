62a4f4b2c83a0558cd82003ee169741a05c059d1
Ernst Stolz <eric@spiritsdontfly.com>
2024-04-07 17:26:52 +0200

    add win states, determine outcome, show outcome

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import scissors from "./assets/scissors.svg";
 import "./App.css";
 
 const options = ["✊", "✋", "✌️"];
+const winStates = [
+  ["✊", "✌️"],
+  ["✌️", "✋"],
+  ["✋", "✊"],
+];
 
 function App() {
   const [player, setPlayer] = useState("");
@@ -15,6 +20,18 @@ function App() {
     }, 2000);
   }
 
+  /* derived state */
+  let outcome = "pending";
+  if (computer != "🎲" && player != computer) {
+    outcome = winStates.some(
+      ([human, npc]) => player === human && computer === npc
+    )
+      ? "win"
+      : "lose";
+  } else if (player === computer) {
+    outcome = "draw";
+  }
+
   return (
     <>
       <div>
@@ -24,6 +41,12 @@ function App() {
       <h2>
         {player} vs. {computer}
       </h2>
+      <div className="card">
+        {outcome === "pending" && <h3>wait for it ...</h3>}
+        {outcome === "win" && <h3>YOU WIN!</h3>}
+        {outcome === "draw" && <h3>DRAW!</h3>}
+        {outcome === "lose" && <h3>YOU LOSE!</h3>}
+      </div>
       <div>
         Choose your option
         {options.map((option) => (
